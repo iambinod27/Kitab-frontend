90dee7b0b7862c13937355ed59821c0d6d444777
MyselfRoshan <roshanghimire699@gmail.com>
2022-10-11 21:15:03 +0545

    feat: added authorprofile page

--- assets/js/slickCarousel.js
+++ assets/js/slickCarousel.js
@@ -75,3 +75,43 @@ $(".popular-carousel").slick({
     // instead of a settings object
   ],
 });
+
+// Author Profile
+// Author Book section
+$(".author__books-carousel").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  nextArrow: ".author__books-carousel__btn-left",
+  prevArrow: ".author__books-carousel__btn-right",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 825,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
