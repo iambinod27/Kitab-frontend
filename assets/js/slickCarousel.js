c2d71f933c35c7f52ab336c3054b22b9e8fe4335
iambinod <helloitsbinod@gmail.com>
2022-10-12 14:20:26 +0545

    feat: added authore page

--- assets/js/slickCarousel.js
+++ assets/js/slickCarousel.js
@@ -75,3 +75,43 @@ $(".popular-carousel").slick({
     // instead of a settings object
   ],
 });
+
+// Author Profile
+// Author Book section
+$(".carousel-author__books").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  nextArrow: ".carousel-author__books__btn-left",
+  prevArrow: ".carousel-author__books__btn-right",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 825,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
